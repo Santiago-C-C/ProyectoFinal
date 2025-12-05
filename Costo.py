9e486b7fbb2e8eb72d572119999b8830becbcc21
Santiago-C-C <156542034+Santiago-C-C@users.noreply.github.com>
2025-12-04 21:27:28 -0600

    Add files via upload

--- Costo.py
+++ Costo.py
@@ -42,7 +42,7 @@ LR.fit(X_train, y_train)
 b1 = LR.coef_
 b0 = LR.intercept_
 
-prediccion = (b0+ b1[0]*df["Presupuesto"] + b1[1]*df["Tiempo"] + b1[2]*df["Tipo"]+ b1[3]*["Momento"]+ b1[4]*["No. de personas"])
+prediccion = (b0+ b1[0]*df["Presupuesto"] + b1[1]*df["Tiempo"] + b1[2]*df["Tipo"]+ b1[3]*df["Momento"]+ b1[4]*df["No. de personas"])
 
 st.subheader('Cálculo del costo aproximado')
 st.write('El costo aproximado es: ', prediccion.values[0], "$")
